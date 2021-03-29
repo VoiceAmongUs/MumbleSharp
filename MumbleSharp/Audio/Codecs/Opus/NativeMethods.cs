017838698762a80ce92bee0dd15d8381fc89caff
Christoph Honal <christoph.honal@web.de>
2021-03-30 01:17:40 +0200

    Use proper DLLImport for opus library Remove opus binaries
    Closes #2

--- MumbleSharp/Audio/Codecs/Opus/NativeMethods.cs
+++ MumbleSharp/Audio/Codecs/Opus/NativeMethods.cs
@@ -24,135 +24,49 @@
 // 
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace MumbleSharp.Audio.Codecs.Opus
 {
+
     /// <summary>
     /// Wraps the Opus API.
     /// </summary>
-    public class NativeMethods
+    internal class NativeMethods
     {
 
-        static NativeMethods()
-        {
-            LoadLibrary();
-        }
+        const string LibraryName = "libopus";
 
-        public static bool LoadLibrary(List<string> additionalSearchDirectories = null)
-        {
-            var searchDirectories = new List<string>
-            {
-                AppDomain.CurrentDomain.BaseDirectory,
-                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Audio", "Codecs", "Opus", "Libs", Environment.Is64BitProcess ? "64bit" : "32bit")
-            };
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr opus_encoder_create(int sampleRate, int channelCount, int application, out IntPtr error);
 
-            if (additionalSearchDirectories != null)
-            {
-                searchDirectories.AddRange(additionalSearchDirectories);
-            }
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void opus_encoder_destroy(IntPtr encoder);
 
-            foreach (var searchDirectory in searchDirectories)
-            {
-                string libraryPath;
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int opus_encode(IntPtr encoder, IntPtr pcm, int frameSize, IntPtr data, int maxDataBytes);
 
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                {
-                    libraryPath = Path.Combine(searchDirectory, "libopus.so");
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                {
-                    libraryPath = Path.Combine(searchDirectory, "libopus.dylib");
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    libraryPath = Path.Combine(searchDirectory, "opus.dll");
-                }
-                else
-                {
-                    throw new PlatformNotSupportedException();
-                }
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr opus_decoder_create(int sampleRate, int channelCount, out IntPtr error);
 
-                if (File.Exists(libraryPath))
-                {
-                    Load(libraryPath);
-                    return true;
-                }
-            }
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void opus_decoder_destroy(IntPtr decoder);
 
-            return false;
-        }
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int opus_decode(IntPtr decoder, IntPtr data, int len, IntPtr pcm, int frameSize, int decodeFec);
 
-        private static void Load(string libraryPath)
-        {
-            var image = LibraryLoader.Load(libraryPath);
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int opus_packet_get_nb_channels(IntPtr data);
 
-            if (image == IntPtr.Zero)
-            {
-                throw new Exception("Couldn't load opus library");
-            }
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int opus_packet_get_nb_samples(IntPtr data, int len, int sampleRate);
 
-            var type = typeof(NativeMethods);
-            foreach (var member in type.GetFields(BindingFlags.Static | BindingFlags.NonPublic))
-            {
-                var methodName = member.Name;
-                if (methodName == "opus_encoder_ctl_out") methodName = "opus_encoder_ctl";
-                var fieldType = member.FieldType;
-                var ptr = LibraryLoader.ResolveSymbol(image, methodName);
-                if (ptr == IntPtr.Zero)
-                    throw new Exception(string.Format("Could not resolve symbol \"{0}\"", methodName));
-                member.SetValue(null, Marshal.GetDelegateForFunctionPointer(ptr, fieldType));
-            }
-        }
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int opus_encoder_ctl(IntPtr encoder, Ctl request, int value);
 
-        // ReSharper disable InconsistentNaming
-        // ReSharper disable UnassignedField.Compiler
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "opus_encoder_ctl")]
+        internal static extern int opus_encoder_ctl_out(IntPtr encoder, Ctl request, out int value);
 
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate IntPtr opus_encoder_create_delegate(int sampleRate, int channelCount, int application, out IntPtr error);
-        internal static opus_encoder_create_delegate opus_encoder_create;
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate void opus_encoder_destroy_delegate(IntPtr encoder);
-        internal static opus_encoder_destroy_delegate opus_encoder_destroy;
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate int opus_encode_delegate(IntPtr encoder, IntPtr pcm, int frameSize, IntPtr data, int maxDataBytes);
-        internal static opus_encode_delegate opus_encode;
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate IntPtr opus_decoder_create_delegate(int sampleRate, int channelCount, out IntPtr error);
-        internal static opus_decoder_create_delegate opus_decoder_create;
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate void opus_decoder_destroy_delegate(IntPtr decoder);
-        internal static opus_decoder_destroy_delegate opus_decoder_destroy;
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate int opus_decode_delegate(IntPtr decoder, IntPtr data, int len, IntPtr pcm, int frameSize, int decodeFec);
-        internal static opus_decode_delegate opus_decode;
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate int opus_packet_get_nb_channels_delegate(IntPtr data);
-        internal static opus_packet_get_nb_channels_delegate opus_packet_get_nb_channels;
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate int opus_packet_get_nb_samples_delegate(IntPtr data, int len, int sampleRate);
-        internal static opus_packet_get_nb_samples_delegate opus_packet_get_nb_samples;
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate int opus_encoder_ctl_delegate(IntPtr encoder, Ctl request, int value);
-        internal static opus_encoder_ctl_delegate opus_encoder_ctl;
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate int opus_encoder_ctl_out_delegate(IntPtr encoder, Ctl request, out int value);
-        internal static opus_encoder_ctl_out_delegate opus_encoder_ctl_out;
-
-        // ReSharper restore UnassignedField.Compiler
-        // ReSharper restore InconsistentNaming
 
         internal enum Ctl
         {
@@ -173,5 +87,7 @@ namespace MumbleSharp.Audio.Codecs.Opus
             InvalidState = -6,
             AllocFail = -7
         }
+
     }
+
 }
